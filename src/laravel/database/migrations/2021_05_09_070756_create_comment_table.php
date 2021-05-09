2b85a0169d9a2169874ad51efeb69492315c8ad0
tis1116 <shinoda1116.miwa@gmail.com>
2021-05-09 16:12:03 +0900

    comment_tableのマイグレーションファイル実装

---
+++ src/laravel/database/migrations/2021_05_09_070756_create_comment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment', function (Blueprint $table) {
+            $table->uuid('comment_id')->primary();
+            $table->string('post_id');
+            $table->string('user_id');
+            $table->text('content');
+            $table->date('comment_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment');
+    }
+}
