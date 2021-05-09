c2c4da9e2c94b98f5a9eaaa9fa3283ec1d6bcabd
tis1116 <shinoda1116.miwa@gmail.com>
2021-05-09 16:06:48 +0900

    post_tableのマイグレーションファイル実装

--- src/laravel/database/migrations/2021_05_09_065439_create_post_table.php
+++ src/laravel/database/migrations/2021_05_09_065439_create_post_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class PreRegisterUserTable extends Migration
+class CreatePostTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class PreRegisterUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('pre_register_user_table', function (Blueprint $table) {
-            $table->id();
-            $table->uuid('token');
-            $table->string('mail');
-            $table->date('created_at');
+        Schema::create('post', function (Blueprint $table) {
+            $table->uuid('post_id')->primary();
+            $table->string('user_id');
+            $table->string('title');
+            $table->text('content');
+            $table->date('post_time');
         });
-
     }
 
     /**
@@ -29,6 +29,6 @@ class PreRegisterUserTable extends Migration
      */
     public function down()
     {
-        Schema::drop('pre_register_user_table');
+        Schema::dropIfExists('post');
     }
 }
---
+++ src/laravel/database/migrations/2021_05_09_070035_create_pre_register_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreRegisterUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pre_register_user', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('token');
+            $table->string('mail');
+            $table->date('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pre_register_user');
+    }
+}
