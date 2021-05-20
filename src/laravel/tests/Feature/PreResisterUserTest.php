2a18d102430bd37350634fd377027be485e15f29
tis1116 <shinoda1116.miwa@gmail.com>
2021-05-20 23:27:48 +0900

    仮会員登録のテストコード実装

---
+++ src/laravel/tests/Feature/PreResisterUserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PreResisterUserTest extends TestCase
+{
+    /**
+     * 仮会員登録の正常系
+     * @param mail メールアドレス
+     * @return null
+     */
+    public function store()
+    {
+        $response = $this->postJson('/api/user/pre_register',['mail' =>'test@gmail.com']);
+        $response->assertStatus(201);
+    }
+
+    /**
+     * 仮会員登録の異常系
+     * メールアドレスのバリデーションに引っかかる
+     * @param mail メールアドレス
+     * @return null
+     */
+    public function testStoreValidationError(){
+        $response = $this->postJson('/api/user/pre_register',['mail' =>'testgmailcom']);
+        $response->assertStatus(400);
+    }
+}
