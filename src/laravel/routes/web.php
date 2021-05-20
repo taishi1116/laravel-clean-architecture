38aca2c01d4eb5e3418d9e03924bbdd12d186c86
tis1116 <shinoda1116.miwa@gmail.com>
2021-05-20 23:23:25 +0900

    ルーティングの書き間違えを修正

--- src/laravel/routes/web.php
+++ src/laravel/routes/web.php
@@ -20,4 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::post('/user/pre_register', [App\Http\Controllers\PreRegisterUserAPIController::class, 'store']);
