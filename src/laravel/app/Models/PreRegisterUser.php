444e1236c3de01ac601233265ef3cd2b772a8a54
tis1116 <shinoda1116.miwa@gmail.com>
2021-06-05 20:04:10 +0900

    eloquentの補完が効かないため、IDE helperの実装

--- src/laravel/app/Models/PreRegisterUser.php
+++ src/laravel/app/Models/PreRegisterUser.php
@@ -4,10 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\PreRegisterUser
+ *
+ * @property int $id
+ * @property string $token
+ * @property string $mail
+ * @property int $is_registered
+ * @property string $created_at
+ * @method static \Illuminate\Database\Eloquent\Builder|PreRegisterUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PreRegisterUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PreRegisterUser query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PreRegisterUser whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PreRegisterUser whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PreRegisterUser whereIsRegistered($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PreRegisterUser whereMail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PreRegisterUser whereToken($value)
+ * @mixin \Eloquent
+ */
 class PreRegisterUser extends Model
 {
     protected $table = 'pre_register_user';
-    protected $fillable = ['token','mail','created_at'];
+    protected $fallible = ['token','mail','created_at'];
 
     // created_atのみ存在するためfalseとする
     public $timestamps = false;
