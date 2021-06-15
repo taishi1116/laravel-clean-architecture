dbce7d2b88d3800f5c3efabb916fde6130a18e47
tis1116 <shinoda1116.miwa@gmail.com>
2021-06-15 22:45:58 +0900

    /Userのルート実装

--- src/laravel/routes/api.php
+++ src/laravel/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestAPIController;
 use App\Http\Controllers\PreRegisterUserAPIController;
 use App\Http\Controllers\VerifyTokenController;
+use App\Http\Controllers\UserAPIController;
 
 
 /*
@@ -30,3 +31,9 @@ Route::post('/user/pre_register', [PreRegisterUserAPIController::class, 'store']
 
 // 仮会員登録のtoken確認
 Route::get('/verify/{token}', [VerifyTokenController::class, 'index']);
+
+// 会員情報
+Route::post('user', [UserAPIController::class, 'store']);
+Route::get('user/{user_id}', [UserAPIController::class, 'show']);
+Route::put('user/{user_id}', [UserAPIController::class, 'update']);
+Route::delete('user', [UserAPIController::class, 'store']);
