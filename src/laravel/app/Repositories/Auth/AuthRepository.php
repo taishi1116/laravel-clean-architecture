f9406035763d8eeca246139c23851601d965b8ff
tis1116 <shinoda1116.miwa@gmail.com>
2021-07-03 17:06:26 +0900

    AuthRepositoryの実装

---
+++ src/laravel/app/Repositories/Auth/AuthRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repositories\Auth;
+
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthRepository implements AuthInterface {
+    public function login($request){
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required','min:8'],
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return response()->json(['message' => 'ログインが完了しました。'], 200);            
+        }
+
+        return response()->json(['message' => 'ユーザーが見つかりません。'], 401);
+    }
+
+    public function logout($request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->json(['message' => 'ログアウトが完了しました'], 200);
+    }
+}
