7001702369a6a28d69d7079e2ba90f54184a9599
tis1116 <shinoda1116.miwa@gmail.com>
2021-07-04 04:36:38 +0900

    認証系のテストコード実装

---
+++ src/laravel/tests/Feature/AuthTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+// use Tests\Feature\User;
+use App\Models\User;
+use Tests\TestCase;
+
+
+class AuthTest extends TestCase
+{
+    //テスト実施後は仮データの削除
+    use RefreshDatabase;
+
+    private $email;
+    private $password;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        $this->email = 'dummy@example.com';
+        $this->password = 'dummyPassword';
+        User::factory()->create(['email' => $this->email,'password'=> Hash::make($this->password)]);
+    }
+
+   
+
+    /**
+     * ログインの正常系
+     * requestsが正常かつ、cookieがsetされている
+     */
+    public function testLoginSuccess()
+    {
+        $request = ['email' => $this->email,'password'=>$this->password];
+        $response = $this->postJson('/api/login',$request);
+        $response->assertStatus(200);
+    }
+    
+    /**
+     * ログインの異常系
+     * 会員情報とrequestsの情報が異なる
+     */
+    public function testLoginFailedInvalidInput()
+    {
+        $request = ['email' => 'failed@example.com','password'=>$this->password];
+        $response = $this->postJson('/api/login',$request);
+        $response->assertStatus(401);
+    }
+    
+    public function testLogout()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->post('/api/logout');
+        $response->assertStatus(200);
+        $this->assertGuest();
+    }
+}
