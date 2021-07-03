2a36f8fd8740ee17d9f1594ab7e18415707a4627
tis1116 <shinoda1116.miwa@gmail.com>
2021-07-04 07:03:13 +0900

    /postsのフォームリクエスト実装

---
+++ src/laravel/app/Http/Requests/PostRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|uuid',
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ];
+    }
+
+    /**
+     * バリデーション失敗時のエラーハンドリング
+     * @param Validator $validator
+     * @throw HttpResponseException
+     * @see PreRegisterRequest::failedValidation()
+     */
+    protected function failedValidation(Validator $validator) {
+        $res = response()->json([
+            'errors' => $validator->errors(),
+        ], 400);
+        throw new HttpResponseException($res);
+    }
+}
