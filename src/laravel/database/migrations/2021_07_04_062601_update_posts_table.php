da0d9d6c0032aca5f6a24e07ac27dea85891b943
tis1116 <shinoda1116.miwa@gmail.com>
2021-07-04 06:41:16 +0900

    postsテーブルのマイグレーション更新

---
+++ src/laravel/database/migrations/2021_07_04_062601_update_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->timestamps();
+            $table->softDeletes();
+            $table->dropColumn('post_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropTimestamps();	
+            $table->dropSoftDeletes();
+            $table->date('post_time');
+        });
+    }
+}
