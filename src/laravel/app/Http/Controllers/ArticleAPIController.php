fb02ff550b7e24b3e754613493d9823454ea451b
tis1116 <shinoda1116.miwa@gmail.com>
2021-07-04 11:07:09 +0900

    ArticleAPIControllerの実装

---
+++ src/laravel/app/Http/Controllers/ArticleAPIController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ArticleRequest;
+use Illuminate\Requests\Article\ArticleRepository;
+
+class ArticleAPIController extends Controller
+{
+    private $article_repository;
+
+    public function __construct(ArticleRepository $article_repository)
+    {
+        $this->article_repository = $article_repository;
+    }
+
+    public function index()
+    {
+        return $this->article_repository->getAllArticles();
+    }
+
+    public function show($article_id)
+    {
+        $this->article_repository->getArticleDetail($article_id);
+    }
+
+    public function store(ArticleRequest $request)
+    {
+        $user_id =$request->input('user_id');
+        $title =$request->input('title');
+        $content =$request->input('content');
+        $this->article_repository->getArticleDetail($user_id,$title,$content);
+    }
+
+    public function update($article_id,ArticleRequest $request)
+    {
+        $title =$request->input('title');
+        $content =$request->input('content');
+        $this->article_repository->getArticleDetail($article_id,$title,$content);
+    }
+    
+    public function delete($article_id)
+    {
+        $this->article_repository->getArticleDetail($article_id);
+    }
+}
