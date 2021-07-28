0acee344d4871d5dff966f4bab73d5ef6e997115
tis1116 <shinoda1116.miwa@gmail.com>
2021-07-28 10:25:15 +0900

    認証周りの作業途中

--- src/laravel/app/Repositories/Auth/AuthRepository.php
+++ src/laravel/app/Repositories/Auth/AuthRepository.php
@@ -4,23 +4,25 @@ namespace App\Repositories\Auth;
 
 use Illuminate\Support\Facades\Auth;
 
-
-class AuthRepository implements AuthInterface {
-    public function login($request){
+class AuthRepository implements AuthInterface
+{
+    public function login($request)
+    {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required','min:8'],
         ]);
 
-        if(Auth::attempt($credentials)){
+        if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return response()->messageAndStatusCode('ログインが完了しました。', 200);            
+            return response()->messageAndStatusCode(['data' => Auth::user()], 200);
         }
 
         return response()->messageAndStatusCode('ユーザーが見つかりません。', 404);
     }
 
-    public function logout($request){
+    public function logout($request)
+    {
         Auth::logout();
 
         $request->session()->invalidate();
