0c7a66f46401e7bed693ea173fc6b08f1845bbe8
tis1116 <shinoda1116.miwa@gmail.com>
2021-08-02 20:17:11 +0900

    不要な実装の削除

--- src/client/src/components/articles/id/index.tsx
+++ src/client/src/components/articles/id/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 import { PagesHeader } from 'src/components/header/PagesHeader';
 import { ArticleDetail } from 'src/components/articles/id/ArticleDetail';
 import { useArticleDetail } from 'src/hooks/articles/id';
 import { Loading } from 'src/components/common/Loading';
 
 export const ArticleDetailUI = () => {
-  const router = useRouter();
   const { articleDetail, loading } = useArticleDetail();
 
   const { title, content, createdAt, updatedAt } = articleDetail;
@@ -17,7 +15,7 @@ export const ArticleDetailUI = () => {
 
   return (
     <>
-      <PagesHeader title="記事詳細" onClick={() => router.back()} />
+      <PagesHeader title="記事詳細" />
       <ArticleDetail title={title} content={content} createdAt={createdAt} updatedAt={updatedAt} />
     </>
   );
